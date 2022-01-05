a11108079e9ed44c5119221d82bc62071366e27c
Ailen Ciordia <aciordia@portfoliopersonal.com>
2022-01-05 15:08:47 -0300

    Cambiar ubicación stories

--- src/stories/components/Button.stories.tsx
+++ src/stories/components/Button.stories.tsx
@@ -1,9 +1,9 @@
 import { storiesOf } from '@storybook/react-native';
 import React from 'react';
 import { View } from 'react-native';
-import Button from '../components/Button';
-import { ThemeProvider } from '../helpers/ThemeManager';
-import Wrapper from '../helpers/Wrapper';
+import Button from '../../components/Button';
+import { ThemeProvider } from '../../helpers/ThemeManager';
+import Wrapper from '../../helpers/Wrapper';
 
 storiesOf('Button', module)
   .addDecorator(getStory => (
@@ -13,28 +13,25 @@ storiesOf('Button', module)
   ))
   .add('Primary', () => (
     <View>
-      <Button onPress={() => { }} variant="primary">
+      <Button onPress={() => {}} variant="primary">
         Default size
       </Button>
-      <Button
-        onPress={() => { }}
-        variant="primary"
-        size="small"
-        containerStyle={{ marginTop: 10 }}>
+      <Button onPress={() => {}} variant="primary" size="small" containerStyle={{ marginTop: 10 }}>
         Small size
       </Button>
     </View>
   ))
   .add('Secondary', () => (
     <View>
-      <Button onPress={() => { }} variant="secondary">
+      <Button onPress={() => {}} variant="secondary">
         Default size
       </Button>
       <Button
-        onPress={() => { }}
+        onPress={() => {}}
         variant="secondary"
         size="small"
-        containerStyle={{ marginTop: 10 }}>
+        containerStyle={{ marginTop: 10 }}
+      >
         Small size
       </Button>
     </View>
